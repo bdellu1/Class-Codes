b35d016a9cf3123a4c8c2c50d3c1156faca5c29c
bdellu1 <bdellucky@gmail.com>
2016-03-27 13:45:04 -0500

    Create dice.h

---
+++ dice.h
@@ -0,0 +1,14 @@
+ #ifndef DICE_H
+ #define DICE_H
+ #include <iostream>
+ #include <iomanip>
+ #include <cstdlib>
+ #include <ctime>
+ #include <cmath>
+    
+ namespace di{
+   int dice_roll(int number, int sides);
+   float pi(int n);
+ };
+   
+ #endif
